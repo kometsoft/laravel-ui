c4a93267b73fce17213d5e01dc1cc3072c27817f
aerrata <0x0976@gmail.com>
2023-06-22 15:44:44 +0800

    feat: add new css file for datatable

--- src/LaravelUi.php
+++ src/LaravelUi.php
@@ -51,6 +51,7 @@ class LaravelUi
 
         if (in_array('datatables.net', $features)) {
             $link('datatables.net/css/datatables.min.css');
+            $link('datatables.net/css/app.css');
             $script('datatables.net/js/datatables.min.js');
             $script('datatables.net/js/dataTables.buttons.js');
             $script('datatables.net/js/dataTables.renderers.js');
@@ -59,56 +60,4 @@ class LaravelUi
 
         return $imports;
     }
-
-    //     public function generate_imports($features)
-    //     {
-    //         $imports = '';
-
-    //         $asset_path = function ($path) {
-    //             return asset("vendor/ui/$path");
-    //         };
-
-    //         if (in_array('tabler-core', $features)) {
-    //             $imports .= <<<HTML
-    // <link href="{$asset_path('tabler-core/css/tabler.min.css')}" rel="stylesheet">
-    // <link href="{$asset_path('tabler-core/css/tabler-vendors.min.css')}" rel="stylesheet">
-    // <link href="{$asset_path('tabler-core/css/demo.min.css')}" rel="stylesheet">
-    // <script src="{$asset_path('tabler-core/js/demo-theme.min.js')}" defer></script>
-    // <script src="{$asset_path('tabler-core/js/tabler.min.js')}" defer></script>
-    // <script src="{$asset_path('tabler-core/js/demo.min.js')}" defer></script>
-    // HTML;
-    //         }
-
-    //         if (in_array('tabler-icons', $features)) {
-    //             $imports .= <<<HTML
-    // <link href="{$asset_path('tabler-icons/tabler-icons.min.css')}" rel="stylesheet">
-    // HTML;
-    //         }
-
-    //         if (in_array('apexcharts', $features)) {
-    //             $imports .= <<<HTML
-    // <script src="{$asset_path('tabler-core/libs/apexcharts/apexcharts.min.js')}" defer></script>
-    // HTML;
-    //         }
-
-    //         if (in_array('tom-select', $features)) {
-    //             $imports .= <<<HTML
-    // <script src="{$asset_path('tabler-core/libs/tom-select/tom-select.base.min.js')}" defer></script>
-    // HTML;
-    //         }
-
-    //         if (in_array('litepicker', $features)) {
-    //             $imports .= <<<HTML
-    // <script src="{$asset_path('tabler-core/libs/litepicker/litepicker.js')}" defer></script>
-    // HTML;
-    //         }
-
-    //         if (in_array('chart.js', $features)) {
-    //             $imports .= <<<HTML
-    // <script src="{$asset_path('chart.js/chart.min.js')}" defer></script>
-    // HTML;
-    //         }
-
-    //         return $imports;
-    //     }
 }
